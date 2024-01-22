84f41559eb3cb71ebf92fbdd0101412b3a2419f8
Senzenjani <smanjawira@gmail.com>
2024-01-22 12:33:29 +0200

    Added a proxy logic to help in solving the issues of CORS am getting with vanilla javascript

--- app/controllers/api/v1/proxy_controller.rb
+++ app/controllers/api/v1/proxy_controller.rb
@@ -2,21 +2,52 @@
 require 'httparty'
 
 class Api::V1::ProxyController < ApplicationController
+   
+    skip_before_action :verify_authenticity_token, only: [:proxy]
+
     def proxy
-        # Get the target URL from the request parameters or wherever you need it
-    
+        # Get parameters from the request
         target_path = params[:path] || ''
+        target_url = "https://restful-booker.herokuapp.com/#{target_path}"
+    
+        token = params[:token] || ''
+        request_method = params[:method]&.upcase || 'GET' # Default to GET if method is not specified
 
-        target_url = "https://restful-booker.herokuapp.com#{target_path}"
-
+        request_params = params[:proxy] || {} # Use the "proxy" parameters directly as the request body
+    
         # Make the request to the external API using HTTParty
-        response = HTTParty.get(target_url, headers: request.headers)
-
+        response = case request_method
+                   when 'GET'
+                     HTTParty.get(target_url, headers: request_headers(token), query: request_params)
+                   when 'POST'
+                     HTTParty.post(target_url, headers: request_headers(token), body: request_params.to_json)
+                   when 'PUT'
+                     HTTParty.put(target_url, headers: request_headers(token), body: request_params.to_json)
+                   when 'DELETE'
+                     HTTParty.delete(target_url, headers: request_headers(token), body: request_params.to_json)
+                   else
+                     # Unsupported request method
+                     render json: { error: 'Unsupported request method' }, status: :unprocessable_entity
+                     return
+                   end
+    
         # Forward the external API response back to the original requester
+       # Forward only the status and JSON body of the external API response back to the original requester
         render json: {
             status: response.code,
-            headers: response.headers,
-            body: response.body
+            body: JSON.parse(response.body) # Parse the JSON body to ensure it's valid JSON
         }
-    end
+
+      end
+    
+      private
+    
+      def request_headers(token)
+        {
+          'Authorization' => "Bearer #{token}",
+          'Content-Type' => 'application/json',
+          'User-Agent' => request.headers['User-Agent']
+          # Add other headers as needed
+        }
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   Rails.application.routes.draw do
-    get '/proxy', to: 'proxy#proxy'
+    namespace:api do
+      namespace:v1 do 
+        post '/proxy', to: 'proxy#proxy'
+      end
+    end
   end
 end
